ec54ee63de04b9fc6f285a9cdfaf43f30b973d76
srvds8080 <srvds@yandex.ru>
2021-02-11 02:22:19 +0300

    feat: Подключить роуты не закрытые авторизацией

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const usersRoutes = require('./usersRoutes');
 const moviesRoutes = require('./moviesRoutes');
+const auth = require('../middlewares/auth');
+const { signup, signin } = require('../controllers/users');
 
+router.post('/signin/', signin);
+router.post('/signup', signup);
+router.use(auth);
 router.use('/users/', usersRoutes);
 router.use('/movies/', moviesRoutes);
 
