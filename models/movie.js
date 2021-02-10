5c3fdf09b915b0587efdaaef638a14ce4ec94921
srvds8080 <srvds@yandex.ru>
2021-02-10 22:12:13 +0300

    feat: Создать схемы и модели БД

---
+++ models/movie.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movie = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+    },
+  },
+  owner: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('card', movie);
