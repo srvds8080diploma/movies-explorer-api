83fb64d6829d2968cda6afdbeb23080453ea7d02
srvds8080 <srvds@yandex.ru>
2021-02-11 02:21:37 +0300

    feat: Добавить блокирующий контроллер аутентификации

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    const error = new UnauthorizedError('Необходима авторизация');
+    return next(error);
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    const error = new UnauthorizedError('Необходима авторизация');
+    return next(error);
+  }
+  req.user = payload;
+
+  return next();
+};
