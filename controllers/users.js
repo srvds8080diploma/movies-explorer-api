750bdb4f79eb470e2c3c0ff55239c11fa4b7501a
srvds8080 <srvds@yandex.ru>
2021-02-11 02:20:59 +0300

    feat: Создать контроллеры авторизации и аутентификации

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,16 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 const NotFoundError = require('../errors/NotFoundError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+const ConflictError = require('../errors/ConflictError');
 const {
   OK_CODE,
+  CREATE_CODE,
 } = require('../utils/constants');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUser = (req, res, next) => {
   User.findById(req.user._id)
@@ -26,7 +34,52 @@ const updateUser = (req, res, next) => {
     })
     .catch(next);
 };
+const signup = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        throw new ConflictError('Пользователь существует');
+      }
+      return bcrypt.hash(password, 10)
+        .then((hash) => {
+          User.create({
+            name,
+            email,
+            password: hash,
+          })
+            .then(() => {
+              res.status(CREATE_CODE).send({ message: 'Пользователь успешно создан!' });
+            })
+            .catch(next);
+        });
+    })
+    .catch(next);
+};
+const signin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' },
+      );
+      res.send({ token });
+    })
+    .catch(() => {
+      const error = new UnauthorizedError('email или пароль неверные');
+      next(error);
+    });
+};
+
 module.exports = {
   getUser,
   updateUser,
+  signin,
+  signup,
 };
