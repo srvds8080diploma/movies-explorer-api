6a7c7308fa59b5d33738ca4fa127f37438e428cc
srvds8080 <srvds@yandex.ru>
2021-02-11 12:52:47 +0300

    refactor: Переместить вспомогательные роуты в роутинг-файл

--- app.js
+++ app.js
@@ -16,20 +16,8 @@ mongoose.connect(PATH_DB, {
   useUnifiedTopology: true,
 });
 app.use(bodyParser.json());
-
 app.use(requestLogger);
-
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 app.use(routes);
-app.use((req, res, next) => {
-  res.status(404).send({ error: 'Not found' });
-  next();
-});
-
 app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,18 @@ const moviesRoutes = require('./moviesRoutes');
 const auth = require('../middlewares/auth');
 const { signup, signin } = require('../controllers/users');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signin/', signin);
-router.post('/signup', signup);
+router.post('/signup/', signup);
 router.use(auth);
 router.use('/users/', usersRoutes);
 router.use('/movies/', moviesRoutes);
-
+router.use((req, res, next) => {
+  res.status(404).send({ error: 'Not found' });
+  next();
+});
 module.exports = router;
