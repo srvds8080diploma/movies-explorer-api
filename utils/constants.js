b3da14bfae3983a30a00b7239e05017e19dc23cb
srvds8080 <srvds@yandex.ru>
2021-02-14 00:31:02 +0300

    refactor: Вынести опции для mongo и cors из app,js

--- utils/constants.js
+++ utils/constants.js
@@ -3,9 +3,24 @@ const OK_CODE = 200;
 const CREATE_CODE = 201;
 const INTERNAL_SERVER_ERROR_CODE = 500;
 
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: 'GET, PUT, PATCH, POST, DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+const mongooseConnectionOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
 module.exports = {
   REGEX_URL,
   OK_CODE,
   CREATE_CODE,
   INTERNAL_SERVER_ERROR_CODE,
+  corsOptions,
+  mongooseConnectionOptions,
 };
