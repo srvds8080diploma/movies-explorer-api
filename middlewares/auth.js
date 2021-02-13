f3fafa752498fde3660073dcef16e404dde003c3
srvds8080 <srvds@yandex.ru>
2021-02-14 00:22:14 +0300

    refactor: Упростить логику подстановки секретной строки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     const error = new UnauthorizedError('Необходима авторизация');
     return next(error);
