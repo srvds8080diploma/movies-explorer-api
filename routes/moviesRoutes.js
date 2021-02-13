dfc4b77cfa3959c8d7b0d53ca2a73e5329e21990
srvds8080 <srvds@yandex.ru>
2021-02-13 11:10:58 +0300

    feat: Применить к роутам опции валидации celebrate

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
+const { celebrate } = require('celebrate');
+
 const {
   getMovies,
   createMovie,
   deleteMovie,
 } = require('../controllers/movies');
+const { optionsValidCreateMovie } = require('../utils/optionsCelebrate');
+const { optionsValidDeleteMovie } = require('../utils/optionsCelebrate');
 
-router.get('/', getMovies); // возвращает все сохраненные пользователем фильиы
-
-router.post('/', createMovie);
-router.delete('/:id', deleteMovie);
+router.get('/', getMovies);
+router.post('/', celebrate(optionsValidCreateMovie), createMovie);
+router.delete('/:id', celebrate(optionsValidDeleteMovie), deleteMovie);
 
 module.exports = router;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const { celebrate } = require('celebrate');
 const {
   updateUser,
   getUser,
 } = require('../controllers/users');
+const { optionsValidUpdateUser } = require('../utils/optionsCelebrate');
 
 router.get('/me', getUser);
-router.put('/me', updateUser);
+router.put('/me', celebrate(optionsValidUpdateUser), updateUser);
 
 module.exports = router;
