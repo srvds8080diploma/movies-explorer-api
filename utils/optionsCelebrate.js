526564e831ffa5e3d4d2247689cb0dc120dd0b48
srvds8080 <srvds@yandex.ru>
2021-02-14 21:44:44 +0300

    fix: Убрать true из required

--- utils/optionsCelebrate.js
+++ utils/optionsCelebrate.js
@@ -4,13 +4,13 @@ const { REGEX_URL } = require('./constants');
 
 const optionsValidSign = {
   body: Joi.object().keys({
-    email: Joi.string().required(true).email().messages({
+    email: Joi.string().required().email().messages({
       'string.base': 'Недопустимый формат Email',
       'string.empty': 'Недопустимый формат Email',
       'string.email': 'Недопустимый формат Email',
       'any.required': 'Email обязателен',
     }),
-    password: Joi.string().required(true).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
       .messages({
         'string.pattern': 'Недопустимый формат пароля',
         'any.required': 'Пароль обязателен',
@@ -35,42 +35,42 @@ const optionsValidUpdateUser = {
 
 const optionsValidCreateMovie = {
   body: Joi.object().keys({
-    country: Joi.string().required(true).min(2).messages({
+    country: Joi.string().required().min(2).messages({
       'string.min': 'название страны слишком короткое',
       'any.required': 'название страны обязательно',
     }),
-    director: Joi.string().required(true).min(2).messages({
+    director: Joi.string().required().min(2).messages({
       'string.min': 'Имя режиссера слишком короткое',
       'string.base': 'Недопустимый формат',
       'string.empty': 'Недопустимый формат',
       'any.required': 'Имя режиссера обязательно',
     }),
-    duration: Joi.number().required(true),
-    year: Joi.string().required(true).messages({
+    duration: Joi.number().required(),
+    year: Joi.string().required().messages({
       'string.integer': 'Не допустимый формат года',
       'string.base': 'Недопустимый формат',
       'string.empty': 'Недопустимый формат',
       'any.required': 'год обязательный параметр',
     }),
-    description: Joi.string().required(true),
-    image: Joi.string().required(true).pattern(new RegExp(REGEX_URL))
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(new RegExp(REGEX_URL))
       .messages({
         'string.pattern': 'Не допустимый формат ссылки',
         'any.required': 'image обязательно',
       }),
-    trailer: Joi.string().required(true).pattern(new RegExp(REGEX_URL))
+    trailer: Joi.string().required().pattern(new RegExp(REGEX_URL))
       .messages({
         'string.pattern': 'Не допустимый формат ссылки',
         'any.required': 'image обязательно',
       }),
-    nameRU: Joi.string().required(true),
-    nameEN: Joi.string().required(true),
-    thumbnail: Joi.string().required(true).pattern(new RegExp(REGEX_URL))
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().pattern(new RegExp(REGEX_URL))
       .messages({
         'string.pattern': 'Не допустимый формат ссылки',
         'any.required': 'thumbnail обязательно',
       }),
-    movieId: Joi.string().required(true).length(24).hex()
+    movieId: Joi.string().required().length(24).hex()
       .messages({
         'string.length': 'Не допустимый формат Id',
         'string.empty': 'Недопустимый формат Id',
@@ -81,7 +81,7 @@ const optionsValidCreateMovie = {
 
 const optionsValidDeleteMovie = {
   params: Joi.object().keys({
-    id: Joi.string().required(true).length(24).hex()
+    id: Joi.string().required().length(24).hex()
       .messages({
         'string.length': 'Неверный формат переданного запроса',
         'string.hex': 'Неверный формат переданного запроса',
